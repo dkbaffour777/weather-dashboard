8b2797699f54a2437d299194e6c37312e60e642b
Daniel <dkbaffour777@gmail.com>
2021-04-21 11:20:30 -0700

    add search and local storage functionalites, update Readme.md

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # weather-dashboard
+  Deployed application - https://dkbaffour777.github.io/weather-dashboard/
+  
+  - Get the current and future five-day weather conditions for multiple cities
+  - Search history is stored and can be used later on as a shortcut to view previous searches
+ 
+  ## A preview of weather-dashboard
+    ![weather-dashboard-preview](./assets/images/appPreview.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,8 @@ var weatherHistory = []
 var localWeatherHistory = JSON.parse(localStorage.getItem("localWeatherHistory")) 
 if(localWeatherHistory){
     //console.log(localWeatherHistory)
+    weatherHistory = localWeatherHistory
+    weatherHistory.map(city => addVisitedCity(city))
 }
 
 function loadData(city) {
@@ -36,17 +38,51 @@ function currentWeatherData(city) {
         getUV(weatherData.coord.lon, weatherData.coord.lat)
 
         // Save the city at the weather history section and the local storage
-        saveWeatherHistory(city)
+        saveWeatherHistory(weatherData.name)
     })
-    .catch(function(error){
+    .catch(function(){
         document.querySelector("#city-err").setAttribute("class", "err-msg")
         document.querySelector("#city-err").textContent = "City name '" + city + "', was not found"
         pageDataReset(initialPageData)
     })
 }
 
+// UV index data
+function getUV(lon, lat) {
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon+ "&appid=a0722575280635660ac6cc42752f6d56&units=imperial")
+    .then(function(res){
+        return res.json()
+    })
+    .then(function(uvData){
+        var uvEl = document.querySelector("#uv")
+        var uviValue = uvData.current.uvi
+        uvEl.textContent = uviValue
+        
+        // Color code uv index
+        if(uviValue >= 0 && uviValue <= 2) {
+            uvEl.setAttribute("class", "minimal")
+        } else if(uviValue >= 3 && uviValue <= 4) {
+            uvEl.setAttribute("class", "low")
+        } else if(uviValue >= 5 && uviValue <= 6) {
+            uvEl.setAttribute("class", "moderate")
+        } else if(uviValue >= 7 && uviValue <= 9) {
+            uvEl.setAttribute("class", "high")
+        } else if(uviValue >= 10) {
+            uvEl.setAttribute("class", "very-high")
+        }
+    })
+}
+
+// Customized date
+function getWeatherDate(unix_timeStamp) {
+    var myDate = new Date(unix_timeStamp * 1000)
+    var result = (myDate.getMonth() + 1) + "/" + myDate.getDate() + "/" + myDate.getFullYear()
+    return result
+}
+
 // Icon code image link
 function getWeatherIcon(iconCode, iconElCont) {
+    iconElCont.innerHTML = ""
     var iconEl = document.createElement("img")
     iconEl.setAttribute("src", "http://openweathermap.org/img/wn/"+ iconCode +"@2x.png")
     
@@ -55,33 +91,15 @@ function getWeatherIcon(iconCode, iconElCont) {
     iconEl.style.height = "100%"
 }
 
-// UV index data
-function getUV(lon, lat) {
-    fetch("http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon+ "&appid=a0722575280635660ac6cc42752f6d56&units=imperial")
-    .then(function(res){
-        return res.json()
-    })
-    .then(function(uvData){
-        document.querySelector("#uv").textContent = uvData.value
-    })
-}
-
-// Customized date
-function getWeatherDate(unix_timeStamp) {
-    var myDate = new Date(unix_timeStamp * 1000)
-    var result = myDate.getMonth() + "/" + myDate.getDate() + "/" + myDate.getFullYear()
-    return result
-}
-
 // Render the five day weather elements
 function renderFiveDayWeather(fiveDayData) {
     var fiveDayElCont = document.querySelector("#five-day-cont")
-    var fiveDayEl = null
 
-    
+    fiveDayElCont.innerHTML = ""
+
     for (let i = 8; i <= fiveDayData.length; i += 8) {
         if(i === fiveDayData.length) i = fiveDayData.length - 1
-        fiveDayEl = document.createElement("div")
+        var fiveDayEl = document.createElement("div")
         fiveDayEl.setAttribute("class", "fw")
         fiveDayEl.innerHTML = `
         <h4 class="fw-date">` + getWeatherDate(fiveDayData[i].dt) + `</h4>
@@ -131,30 +149,41 @@ function pageDataReset(initialPageData) {
     }
 }
 
+// Render visited city
+function addVisitedCity(city) {
+    // Create a new element and append it to the page
+    var visitedCityEl = document.createElement("div")
+    visitedCityEl.setAttribute("class", "visited-city")
+    visitedCityEl.textContent = city
+    document.querySelector("#weather-history").append(visitedCityEl)
+
+}
+
 // Save the cities searched
 function saveWeatherHistory(city) {
+    // Check if localWeatherHistory exists
     if(localStorage.getItem("localWeatherHistory")) {
         var weatherH = JSON.parse(localStorage.getItem("localWeatherHistory"))
+        // Check if the city name is found in the array list
         if(weatherH.includes(city)) {
             return
         }
     }
     function save() {
-        // Create a new element and append it to the page
-        var visitedCityEl = document.createElement("div")
-        visitedCityEl.setAttribute("class", "visited-city")
-        visitedCityEl.textContent = city
-        document.querySelector("#weather-history").append(visitedCityEl)
-        
+        // Add the city to the visited city list
+        addVisitedCity(city)
+
         // Store the search value in the local storage
         weatherHistory.push(city)
         localStorage.setItem("localWeatherHistory", JSON.stringify(weatherHistory))    
+        console.log(weatherHistory)
     }
     save()
 }
 
 // Load the weather data when the search button is clicked
-function loadSearchInpData() {
+function loadSearchInpData(event) {
+    event.preventDefault()
     var city = document.querySelector("#searchInp").value
     loadData(city)
 }
@@ -170,8 +199,8 @@ function removeErrMsg() {
     document.querySelector("#city-err").textContent = ""
 }
 
-document.querySelector("#searchBtn").addEventListener("click", loadSearchInpData)
+document.querySelector("#search-form").addEventListener("submit", loadSearchInpData)
 
 document.querySelector("#weather-history").addEventListener("click", loadHistoryData)
 
-document.querySelector("#searchInp").addEventListener("focus", removeErrMsg)
+document.querySelector("#searchInp").addEventListener("change", removeErrMsg)
